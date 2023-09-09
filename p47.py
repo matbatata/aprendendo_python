fdc87003330db5cf6cb5bf3a1bfc5ccdda4f2feb
Matheus <142700606+matbatata@users.noreply.github.com>
2023-09-09 13:40:08 -0300

    Add files via upload
    Foi meio difícil fazer esse comando mas deu certo ,_,

---
+++ p47.py
@@ -0,0 +1,39 @@
+# variando 
+a = int(input("digite um número: "))
+
+#elifandu
+if a == 0: 
+    print("zero")
+
+elif a == 1:
+    print("um")
+
+elif a == 2:
+    print("dois")
+
+elif a == 3:
+    print("três")
+
+elif a == 4:
+    print("quatro")
+
+elif a == 5:
+    print("cinco")
+
+elif a == 6:
+    print("seis")
+
+elif a == 7:
+    print("sete")
+
+elif a == 8:
+    print("oito")
+
+elif a == 9:
+    print("nove")
+
+elif a == 10:
+    print("dez")
+
+else:
+    print("digite de 1 a 10 seu bebê")
